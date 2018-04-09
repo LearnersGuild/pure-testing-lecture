3b09b832ec18a4d6ff6be89dad9c90777591d7b5
jeffreywescott <jeffrey@learnersguild.org>
2018-04-09 10:24:33 -0700

    added JSON fetch with tests

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,7 +1,31 @@
-import test from 'tape-async'
-import {theAnswer} from '../index'
+import test from 'blue-tape'
+import nock from 'nock'
 
-test('theAnswer', t => {
-  t.equal(theAnswer(), 42)
+import {jsonFetch} from '../index'
+
+test('jsonFetch', t => {
+  t.plan(2)
+
+  t.test('should fail on error responses', tt => {
+    nock.cleanAll()
+    nock('https://www.example.com')
+      .get('/NotFound')
+      .reply(404, 'Not Found')
+      .get('/ServerError')
+      .reply(500, 'Internal Server Error')
+    tt.shouldFail(jsonFetch('https://www.example.com/NotFound'), 'Not Found')
+    tt.shouldFail(jsonFetch('https://www.example.com/ServerError'), 'Internal Server Error')
+  })
+
+  t.test('should return the data as an object on success response', async tt => {
+    const expectedData = {this: 'is', my: 'data'}
+    nock.cleanAll()
+    nock('https://www.example.com')
+      .get('/SomeData')
+      .reply(200, expectedData)
+    const data = await jsonFetch('https://www.example.com/SomeData')
+    tt.deepEqual(data, expectedData)
+  })
+
   t.end()
 })
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-export const theAnswer = () => 42
+import fetch from 'node-fetch'
+
+export const jsonFetch = (url, options = {}) => {
+  const allOptions = {...options, Accept: 'application/json'}
+  return fetch(url, allOptions)
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(`${resp.status}: ${resp.statusText || 'Unknown Error'}`)
+      }
+      return resp.json()
+    })
+}
+
+export const getTopCryptoCoins = (numCoins = 10) => {
+  const url = `https://api.coinmarketcap.com/v1/ticker/?limit=${numCoins}`
+  return jsonFetch(url)
+}
